7f17b54e69725de976b46f82a61afbff45085110
lgjohann <guilherme4695@gmail.com>
2024-09-20 15:23:53 -0300

    obtenndo um usuario por id

--- src/main/java/com/johann/swampAPI/services/UserService.java
+++ src/main/java/com/johann/swampAPI/services/UserService.java
@@ -1,12 +1,14 @@
 package com.johann.swampAPI.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.johann.swampAPI.domain.User;
 import com.johann.swampAPI.repository.UserRepository;
+import com.johann.swampAPI.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,10 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 	
 }
