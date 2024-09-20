ce02ac5512f9d195304e3c9a4ca039952810f842
lgjohann <guilherme4695@gmail.com>
2024-09-19 21:54:49 -0300

    Instanciacao da base de dados

---
+++ src/main/java/com/johann/swampAPI/SwampApiApplication.java
@@ -0,0 +1,13 @@
+package com.johann.swampAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwampApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwampApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johann/swampAPI/SwampApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johann.swampAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwampApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
